e11caa04f63d33e5c88962a813ba926568d79af1
SiRahXCX <abanggimomin02@gmail.com>
2026-05-18 15:17:33 +0530

    designed and implemented home page for PrintForge

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,50 @@
-import Image from "next/image";
+import Image from 'next/image'
+import modelImage from '@/app/assets/images/3d_models.png'
+import logo from '@/app/assets/images/logo.png'
 
-export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+function NavBar() {
+    return (
+        <header className="flex justify-between items-center px-10 h-21.75 mb-10">
+            <div className="w-60 h-15.25">
+                <Image 
+                    src={logo}
+                    alt="Printforge logo"
+                />
+            </div>
+            <nav>
+                <ul className="flex list-none">
+                    <li className="font-mono text-sm text-[#606060] font-semibold ">3D MODELS</li>
+                    <li className="font-mono text-sm text-[#606060] font-semibold ms-10">ABOUT</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default function HomePage() {
+    return (
+        <>
+            <NavBar /> 
+            <main>
+                <div className="flex justify-evenly items-center">
+                    <div className="w-150 h-113">
+                        <div className="mb-18.25">
+                            <div className="text-base font-medium mb-5">YOUR GO-TO PLATFORM FOR 3D PRINTING FILES</div>
+                            <div className="text-[56px] font-bold leading-none mb-5">Discover what's possible with 3D printing</div>
+                            <div className="text-[28px] font-regular leading-none">Join our community of creators and explore a vast library of user-submitted models.</div>
+                        </div>
+                        <button type="button" className="text-lg font-semibold border-2 p-3">BROWSER MORE</button>
+                    </div>
+                    <div className="ms-25">
+                        <Image 
+                            src={modelImage}
+                            alt="Splash screen"
+                            width={627}
+                            height={627}
+                        />
+                    </div>
+                </div>
+            </main>
+        </>
+    );
 }
